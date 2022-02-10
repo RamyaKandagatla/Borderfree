43d0460de3fc88e8a641b3a16e6121de678a5f34
RAMYA <kandagatlaramya5@gmail.com>
2022-02-10 13:48:07 +0530

    setup database nodejs and react

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+const dotenv=require('dotenv');
+dotenv.config();
+require('./database/databaseconnection');
+
+
+// app.get('/',(req,res)=>{
+//     return res.send("hello world");
+// })
+app.listen(process.env.PORT,()=>{
+    console.log(`serve is running at ${process.env.PORT}`);
+});
